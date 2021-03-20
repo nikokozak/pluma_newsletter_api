e91ef9abab0fecba24298b70fcdb70b92eee3981
Nikolai Kozak <nikokozak@gmail.com>
2021-03-20 17:06:51 -0300

    add new_subscriber test to subscriber controller tests

--- test/pluma_api_web/controllers/subscriber_controller_test.exs
+++ test/pluma_api_web/controllers/subscriber_controller_test.exs
@@ -1,10 +1,11 @@
 defmodule PlumaApiWeb.SubscriberControllerTest do
   use PlumaApiWeb.ConnCase
+  use PlumaApi.TestUtils
   alias PlumaApi.Subscriber
   alias PlumaApi.Repo
 
   @moduletag :subscriber_controller_tests
-
+  @list_id Keyword.get(Application.get_env(:pluma_api, :mailchimp), :main_list_id)
 
   # Tests both ?rid and ?email params
   test "get_subscriber/2", %{conn: conn} do
@@ -31,19 +32,31 @@ defmodule PlumaApiWeb.SubscriberControllerTest do
     assert Jason.decode!(conn_rid_failure.resp_body) == %{ "errors" => %{ "detail" => "Could not find subscriber" } }
   end
 
-  test "new_subscriber/2", %{conn: conn} do
+  @tag test_sub: PlumaApi.Factory.subscriber()
+  test "new_subscriber/2", %{conn: conn, test_sub: test_sub} do
+    conn_new = post(conn, Routes.subscriber_path(conn, :new_subscriber), make_new_subscriber_call(test_sub))
 
+    assert json_response(conn_new, 200)
+    assert %{"status" => _status, "detail" => _detail, "stage" => _stage} = Jason.decode!(conn_new.resp_body)
+    assert PlumaApi.MailchimpRepo.check_exists(test_sub.email, @list_id)
+
+    conn_existing = post(conn, Routes.subscriber_path(conn, :new_subscriber), make_new_subscriber_call(test_sub))
+
+    assert json_response(conn_existing, 400)
+    assert %{"status" => _status, "detail" => _detail, "stage" => _stage} = Jason.decode!(conn_new.resp_body)
+
+    conn_invalid = post(conn, Routes.subscriber_path(conn, :new_subscriber), %{"fname" => "", "lname" => "", "email" => "not_an_email", "rid" => "111111", "prid" => ""})
+
+    assert json_response(conn_invalid, 400)
   end
 
-
-
-  defp make_new_subscriber_call(prid \\ "") do
+  defp make_new_subscriber_call(factory_sub) when is_map(factory_sub) do
     %{
       "fname" => Faker.Person.En.first_name(),
       "lname" => Faker.Person.En.last_name(),
-      "email" => Faker.Internet.email(),
-      "rid" => Nanoid.generate(10),
-      "prid" => prid,
+      "email" => factory_sub.email,
+      "rid" => factory_sub.rid,
+      "prid" => factory_sub.parent_rid,
       "ip_signup" => Faker.Internet.ip_v4_address()
     }
   end

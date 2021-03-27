b03cad8b81e346903981adb857ef26fd8e14047f
Nikolai Kozak <nikokozak@gmail.com>
2021-03-27 19:57:46 -0300

    adjust name regex to accommodate periods

--- lib/pluma_api/subscriber.ex
+++ lib/pluma_api/subscriber.ex
@@ -9,8 +9,8 @@ defmodule PlumaApi.Subscriber do
   @foreign_key_type :binary_id
 
   @email_regex ~r/^[^@\s]+@[^@\s]+\.[^@\s]+$/
-  @name_regex ~r/^[\w\s\-']*$/u
-  @rid_regex ~r/^[\w_\-']*$/u
+  @name_regex ~r/^[\w\s\-'\.]*$/u
+  @rid_regex ~r/^[\w_\-'\.]*$/u
 
   @status_types ["pending", "subscribed", "unsubscribed", "cleaned", "archived"]
 
